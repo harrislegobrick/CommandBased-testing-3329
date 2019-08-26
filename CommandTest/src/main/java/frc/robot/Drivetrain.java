f064f07dbcd68428205f7f22f8fb4dd905838e42
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2019-08-26 17:04:31 -0400

    Added gyro calibration
    Gyro was never calibrated; now it is.

---
+++ CommandTest/src/main/java/frc/robot/Drivetrain.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.TankDrive;
+
+/**
+ * Add your docs here.
+ */
+public class Drivetrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private Talon leftMotor, rightMotor;
+  public ADXRS450_Gyro gyro;
+
+  public Drivetrain() {
+    leftMotor = new Talon(RobotMap.leftMotor);
+    rightMotor = new Talon(RobotMap.rightMotor);
+    gyro = new ADXRS450_Gyro();
+    gyro.calibrate();
+  }
+
+  public void setRaw(double leftvalue, double rightvalue) {
+    leftMotor.set(leftvalue);
+    rightMotor.set(rightvalue);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    setDefaultCommand(new TankDrive());
+  }
+
+}
