2e2e69469e287800138554da52eb9bddedba208c
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2019-08-29 17:01:57 -0400

    Conform to OI Joystick rename

--- CommandTest/src/main/java/frc/robot/commands/TankDrive.java
+++ CommandTest/src/main/java/frc/robot/commands/TankDrive.java
@@ -7,7 +7,7 @@
 
 package frc.robot.commands;
 
-import static frc.robot.OI.m_leftStick;
+import static frc.robot.OI.leftStick;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
@@ -27,7 +27,7 @@ public class TankDrive extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    double throttle = (1.0 - m_leftStick.getThrottle()) / -2.0;
+    double throttle = (1.0 - leftStick.getThrottle()) / -2.0;
     Robot.drivetrain.setRaw(Robot.oi.getLeftJoyY() * throttle, Robot.oi.getRightJoyY() * throttle);
   }
 
