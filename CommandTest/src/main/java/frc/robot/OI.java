0f4d98566c36b2f2ce0e9afef9237f043318f004
harrislegobrick <54484642+harrislegobrick@users.noreply.github.com>
2019-08-29 16:58:31 -0400

    Added Lift Pneumatics to the joystick POV

--- CommandTest/src/main/java/frc/robot/OI.java
+++ CommandTest/src/main/java/frc/robot/OI.java
@@ -10,6 +10,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.buttons.POVButton;
 import frc.robot.commands.*;
 import frc.robot.commands.autoncommands.*;
 
@@ -19,22 +20,26 @@ import frc.robot.commands.autoncommands.*;
  */
 public class OI {
   // creating joysticks and buttons
-  public static Joystick m_rightStick = new Joystick(RobotMap.rightStick);
-  public static Joystick m_leftStick = new Joystick(RobotMap.leftStick);
+  public static Joystick rightStick = new Joystick(RobotMap.rightStick);
+  public static Joystick leftStick = new Joystick(RobotMap.leftStick);
   public static Button lTrigger, rTrigger, lBigButton, rBigButton, lTopLeft, lTopRight, rTopLeft, rTopRight, lBottom;
-  public static int povHat;
+  public static POVButton lpovUp, lpovDown, rpovUp, rpovDown;
 
   public static void init() {
     // assigning buttons to names
-    lTrigger = new JoystickButton(m_leftStick, 1);
-    rTrigger = new JoystickButton(m_rightStick, 1);
-    lBigButton = new JoystickButton(m_leftStick, 2);
-    rBigButton = new JoystickButton(m_rightStick, 2);
-    lTopLeft = new JoystickButton(m_leftStick, 3);
-    lTopRight = new JoystickButton(m_leftStick, 4);
-    rTopLeft = new JoystickButton(m_rightStick, 3);
-    rTopRight = new JoystickButton(m_rightStick, 4);
-    lBottom = new JoystickButton(m_leftStick, 5);
+    lTrigger = new JoystickButton(leftStick, 1);
+    rTrigger = new JoystickButton(rightStick, 1);
+    lBigButton = new JoystickButton(leftStick, 2);
+    rBigButton = new JoystickButton(rightStick, 2);
+    lTopLeft = new JoystickButton(leftStick, 3);
+    lTopRight = new JoystickButton(leftStick, 4);
+    rTopLeft = new JoystickButton(rightStick, 3);
+    rTopRight = new JoystickButton(rightStick, 4);
+    lBottom = new JoystickButton(leftStick, 5);
+    lpovUp = new POVButton(leftStick, 0);
+    lpovDown = new POVButton(leftStick, 180);
+    rpovUp = new POVButton(rightStick, 0);
+    rpovDown = new POVButton(rightStick, 180);
 
     // Controlling the lift
     rTrigger.whileHeld(new LiftCommand(LiftCommand.Mode.UP));
@@ -44,35 +49,32 @@ public class OI {
     rBigButton.whileHeld(new IntakeCommand(IntakeCommand.Mode.IN));
     lBigButton.whileHeld(new IntakeCommand(IntakeCommand.Mode.OUT));
 
-    // limelight
+    // Limelight
     lBottom.whileHeld(new LimelightTrackToTarget());
 
-    // pnewmatikz (plus code in the POV command)
+    // Pnewmatikz for hatch
     lTopLeft.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.IN));
     rTopRight.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.OUT));
     lTopRight.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.DOWN));
     rTopLeft.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.UP));
 
+    // Pnewmatikz for climb
+    lpovUp.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.BUP));
+    lpovDown.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.BDOWN));
+    rpovUp.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.FUP));
+    rpovDown.whenPressed(new PneumaticsCommand(PneumaticsCommand.Mode.FDOWN));
+
   }
 
+  // Removes the deadzone from the controllers so it doesn't hurt the motors by switching values rapidly 
   public double getLeftJoyY() {
-    double raw = m_leftStick.getY();
+    double raw = leftStick.getY();
     return Math.abs(raw) < RobotMap.joy_deadzone ? 0.0 : raw;
   }
 
   public double getRightJoyY() {
-    double raw = m_rightStick.getY();
+    double raw = rightStick.getY();
     return Math.abs(raw) < RobotMap.joy_deadzone ? 0.0 : raw;
-  }
-
-  public int getLeftPOV(){
-    int pid = m_leftStick.getPOV(0);
-    return pid;
-  }
-
-  public int getRightPOV(){
-    int pid = m_rightStick.getPOV(0);
-    return pid;
   }
 
 }
