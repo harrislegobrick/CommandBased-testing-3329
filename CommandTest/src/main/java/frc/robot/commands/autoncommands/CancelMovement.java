93f42eae528a20f55528b2d53c01eb0f5ed814d9
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2019-09-20 19:48:08 -0400

    Edited Logic

--- CommandTest/src/main/java/frc/robot/commands/autoncommands/CancelMovement.java
+++ CommandTest/src/main/java/frc/robot/commands/autoncommands/CancelMovement.java
@@ -27,10 +27,10 @@ public class CancelMovement extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    if (Robot.drivetrain.accel.getX() > exactness) {
-      Robot.drivetrain.setRaw(speed, speed);
-    } else {
+    if (Robot.drivetrain.accel.getY() > 0) {
       Robot.drivetrain.setRaw(-speed, -speed);
+    } else {
+      Robot.drivetrain.setRaw(speed, speed);
     }
   }
 
