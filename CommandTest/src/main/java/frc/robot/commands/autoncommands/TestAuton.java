baf06edeb0d122972f44ccc3f5124efbabe31ef0
Harris N <54484642+harrislegobirck@users.noreply.github.com>
2019-09-26 17:37:38 -0400

    Changed Timer to WaitCommand

--- CommandTest/src/main/java/frc/robot/commands/autoncommands/TestAuton.java
+++ CommandTest/src/main/java/frc/robot/commands/autoncommands/TestAuton.java
@@ -8,7 +8,7 @@
 package frc.robot.commands.autoncommands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 public class TestAuton extends CommandGroup {
   /**
@@ -16,7 +16,7 @@ public class TestAuton extends CommandGroup {
    */
   public TestAuton() {
     addSequential(new DriveStraight(2, 0.5));
-    Timer.delay(1);
+    addSequential(new WaitCommand(1));
     //addSequential(new CancelMovement()); 
     addSequential(new RotateBot(90, RotateBot.Direction.LEFT));
     // Add Commands here:
